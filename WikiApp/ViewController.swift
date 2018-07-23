0e2d562750d4db7bf0dd3c33f624f7812ec62b9f
Sachin <sachin@wizely.in>
2018-07-23 23:49:39 +0530

    Initial Commit

---
+++ WikiApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WikiApp
+//
+//  Created by Sachin Nautiyal on 7/23/18.
+//  Copyright © 2018 Sachin Nautiyal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
